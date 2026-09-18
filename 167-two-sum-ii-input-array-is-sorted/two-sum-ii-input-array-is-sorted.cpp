1df88442309844d7172255378528023180a4d461
yashwanthsoft1 <87741915+yashwanthsoft1@users.noreply.github.com>
2026-09-18 17:40:49 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (8.78%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -11,6 +11,8 @@ public:
             if(sum==target){
                 ans.push_back(i+1);
                 ans.push_back(j+1); 
+                return ans;
+
             }
             if(sum>target){
                 j--;
